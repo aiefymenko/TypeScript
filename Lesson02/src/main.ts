fe09ad9037fb389cc9aaf615bd20f34b1178c82a
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-04-23 21:10:04 -0400

    Introducing any type

--- Lesson02/src/main.ts
+++ Lesson02/src/main.ts
@@ -1,3 +1,10 @@
 let myName: string = 'Artem';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
 
 myName = "Joe";
+meaningOfLife = 42;
+isLoading = true;
+album = 'van Halen';
