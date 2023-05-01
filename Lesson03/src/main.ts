f1a56f043ac1ffa1fd1cea9ca15c9a5ca80b9f35
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-04-30 21:40:50 -0400

    Playing around with arrays using TS

--- Lesson03/src/main.ts
+++ Lesson03/src/main.ts
@@ -3,3 +3,9 @@ let stringArr = ['one', 'hey', 'Artem'];
 let guitars = ['Strat', 'Les Paul', 5150];
 
 let mixData = ['EVH', 1984, true];
+
+stringArr[0] = 'John';
+stringArr.push('hey');
+
+guitars[0] = 1994;
+guitars.unshift('Jim');
