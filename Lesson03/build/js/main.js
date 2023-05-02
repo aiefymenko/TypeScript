71471d8c525490421fb519b5a2016791025f3396
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-05-01 21:14:57 -0400

    Creating and predefying type of string inside an array

--- Lesson03/build/js/main.js
+++ Lesson03/build/js/main.js
@@ -1,8 +1,13 @@
 "use strict";
 let stringArr = ['one', 'hey', 'Artem'];
 let guitars = ['Strat', 'Les Paul', 5150];
-let mixData = ['EVH', 1984, true];
+let mixedData = ['EVH', 1984, true];
 stringArr[0] = 'John';
 stringArr.push('hey');
 guitars[0] = 1994;
 guitars.unshift('Jim');
+guitars = stringArr;
+mixedData = guitars;
+let test = [];
+let bands = [];
+bands.push('Van Halen');
--- Lesson03/src/main.ts
+++ Lesson03/src/main.ts
@@ -2,7 +2,7 @@ let stringArr = ['one', 'hey', 'Artem'];
 
 let guitars = ['Strat', 'Les Paul', 5150];
 
-let mixData = ['EVH', 1984, true];
+let mixedData = ['EVH', 1984, true];
 
 stringArr[0] = 'John';
 stringArr.push('hey');
@@ -10,4 +10,9 @@ stringArr.push('hey');
 guitars[0] = 1994;
 guitars.unshift('Jim');
 
-stringArr = guitars;
+guitars = stringArr;
+mixedData = guitars;
+
+let test = [];
+let bands : string[] = [];
+bands.push('Van Halen');
