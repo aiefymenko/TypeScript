ca2414bbbe23fa909e7303a185718ebe0261ac91
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-05-07 18:09:29 -0400

    Create a valid TS solution for JS code

---
+++ Lesson05/src/copyright.ts
@@ -0,0 +1,16 @@
+//Original JS code
+// const year = document.getElementById('year');
+// const thisYear = new Date().getFullYear();
+// year.setAttribute('datetime', thisYear);
+// year.textContext = thisYear;
+
+
+//1st solution
+let year: HTMLElement | null
+year = document.getElementById('year');
+let thisYear: string
+thisYear = new Date().getFullYear().toString();
+if (year) {
+  year.setAttribute('datetime', thisYear);
+  year.textContent = thisYear;
+}
