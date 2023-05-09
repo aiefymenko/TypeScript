ed49fd5b0c26839baab1210960c84a49d5ef286b
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-05-08 21:36:13 -0400

    Create DRY version of class

--- Lesson06/build/js/main.js
+++ Lesson06/build/js/main.js
@@ -1,7 +1,31 @@
 "use strict";
 //basic structure for class component
+// class Coder {
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
+//     constructor(
+//         name: string,
+//         music: string,
+//         age: number,
+//         lang: string) {
+//         this.name = name;
+//         this.music = music;
+//         this.age = age;
+//         this.lang =lang;
+//     }
+// }
+//DRY version of class
 class Coder {
-    constructor(name) {
+    constructor(name, music, age, lang) {
         this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
     }
 }
--- Lesson06/src/main.ts
+++ Lesson06/src/main.ts
@@ -1,8 +1,33 @@
 //basic structure for class component
-class Coder {
-    name: string
+// class Coder {
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
 
-    constructor(name: string) {
+//     constructor(
+//         name: string,
+//         music: string,
+//         age: number,
+//         lang: string) {
+//         this.name = name;
+//         this.music = music;
+//         this.age = age;
+//         this.lang =lang;
+//     }
+// }
+
+//DRY version of class
+class Coder {
+
+    constructor(
+        public readonly name: string,
+        public music: string,
+        private age: number,
+        protected lang: string) {
         this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang =lang;
     }
 }
