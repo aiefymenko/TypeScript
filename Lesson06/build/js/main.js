de6e6e09d470fc1ec1cef7b4e7045caa97e6a348
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-05-08 21:26:09 -0400

    Create a basic TS class component

--- Lesson06/build/js/main.js
+++ Lesson06/build/js/main.js
@@ -1,23 +1,7 @@
 "use strict";
-//convert to mor or less specific
-let a = 'hello';
-let b = a; //less specific type
-let c = a; //more specific
-let d = 'world';
-let e = 'world';
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-let myVal = addOrConcat(2, 2, 'concat');
-//Be careful! TS-sees no problem here but it returns string
-let nextVal = addOrConcat(2, 2, 'concat');
-//double casting or force casting
-10;
-//The DOM
-const img = document.querySelector('img'); //non null assertion
-const myImg = document.getElementById('#img');
-const nextImg = document.getElementById('#img');
-img.src;
-myImg.src;
+//basic structure for class component
+class Coder {
+    constructor(name) {
+        this.name = name;
+    }
+}
--- Lesson06/src/main.ts
+++ Lesson06/src/main.ts
@@ -1,32 +1,8 @@
-type One = string;
-type Two = string | number;
-type Three = 'hello';
+//basic structure for class component
+class Coder {
+    name: string
 
-//convert to mor or less specific
-let a: One = 'hello';
-let b = a as Two; //less specific type
-let c = a as Three //more specific
-
-let d = <One> 'world'
-let e = <string | number> 'world'
-
-const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
-    if (c === 'add') return a + b;
-    return '' + a + b;
+    constructor(name: string) {
+        this.name = name;
+    }
 }
-
-let myVal: string = addOrConcat(2,2,'concat') as string;
-
-//Be careful! TS-sees no problem here but it returns string
-let nextVal: number = addOrConcat(2,2,'concat') as number;
-
-//double casting or force casting
-(10 as unknown) as string;
-
-//The DOM
-const img = document.querySelector('img')!;//non null assertion
-const myImg = document.getElementById('#img')! as HTMLImageElement 
-const nextImg = <HTMLImageElement>document.getElementById('#img')
-
-img.src
-myImg.src
