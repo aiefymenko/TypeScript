93c1de42507b8ce0a78b5065f9b04f0efafc1422
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-05-11 21:43:25 -0400

    Create an example of index signature

--- Lesson07/build/js/main.js
+++ Lesson07/build/js/main.js
@@ -1,6 +1,23 @@
 "use strict";
+// interface TransactionObj {
+//     Pizza: number,
+//     Books: number,
+//     Job: number
+// }
 const todaysTransaction = {
     Pizza: -10,
     Books: -5,
     Job: 50
 };
+console.log(todaysTransaction.Pizza);
+console.log(todaysTransaction['Pizza']);
+let prop = 'Pizza';
+console.log(todaysTransaction[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransaction));
--- Lesson07/src/main.ts
+++ Lesson07/src/main.ts
@@ -1,12 +1,32 @@
 //INDEX SIGNATURES
 interface TransactionObj {
-    Pizza: number,
-    Books: number,
-    Job: number
+    [index: string]: number
 }
+// interface TransactionObj {
+//     Pizza: number,
+//     Books: number,
+//     Job: number
+// }
 
 const todaysTransaction: TransactionObj = {
      Pizza: -10,
      Books: -5,
      Job: 50
 }
+
+console.log(todaysTransaction.Pizza);
+console.log(todaysTransaction['Pizza']);
+
+
+let prop: string = 'Pizza'
+console.log(todaysTransaction[prop]);
+
+const todaysNet = (transactions: TransactionObj): number => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction]
+    }
+    return total;
+}
+
+console.log(todaysNet(todaysTransaction));
