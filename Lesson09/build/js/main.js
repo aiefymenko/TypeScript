f1e9dfa0cd405eb0e1774b8d0d5a387165faf47b
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-05-23 17:18:25 -0400

    Create Required and Readonly types

--- Lesson09/build/js/main.js
+++ Lesson09/build/js/main.js
@@ -10,3 +10,11 @@ const assign1 = {
 };
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded = updateAssignment(assign1, { grade: 95 });
+//Required and Readonly
+const recordAssignment = (assign) => {
+    //send to database etc
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// assignVerified.grade = 88;
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
--- Lesson09/src/main.ts
+++ Lesson09/src/main.ts
@@ -21,3 +21,15 @@ const assign1: Assignment = {
 
 console.log(updateAssignment(assign1, {grade: 95}));
 const assignGraded: Assignment = updateAssignment(assign1, {grade: 95});
+
+//Required and Readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+    //send to database etc
+    return assign
+}
+
+const assignVerified: Readonly<Assignment> = {...assignGraded, verified: true}
+
+// assignVerified.grade = 88;
+recordAssignment({...assignGraded, verified: true})
