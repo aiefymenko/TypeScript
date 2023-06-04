f47b7b0051e9363d5894d9b57f988a8d64bc5778
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-06-03 21:25:09 -0400

    Creadte a class FullList

--- Lesson11/src/model/FullList.ts
+++ Lesson11/src/model/FullList.ts
@@ -8,3 +8,17 @@ interface List {
   addItem(itemObj: ListItem): void,
   removeItem(id: string): void
 }
+
+export default class FullList implements List {
+
+static instance: FullList = new FullList()
+
+  private constructor(
+    private _list: ListItem[] =[]
+  ) {}
+
+  get list(): ListItem[] {
+    return this._list;
+  }
+
+}
